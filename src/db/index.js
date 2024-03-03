ffb04dee7910e3e33aa725f2a7ffa53f354127d2
unknown <pavanrajesh365@gmail.com>
2024-03-03 15:56:39 +0530

    modified env variables

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,3 @@
 import postgres from "postgres";
 
-export const client = postgres(
-  "postgres://postgres.phycoxmvfbhhzsonkgbq:pavanocta123@aws-0-us-west-1.pooler.supabase.com:5432/postgres"
-);
+export const client = postgres(process.env.DATABASE_URL);
